74539defd7808370e35577af16e8083ca74cb99b
aradac <aradac00@fesb.hr>
2021-03-02 10:42:10 +0100

    adding leaderboard

---
+++ Assets/Scripts/PlayFabLogin.cs
@@ -0,0 +1,79 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+
+    private string userEmail;//ovo posli izbaci
+    private string userPassword;//ovo posli izbaci
+    private string username;
+    public GameObject loginCanvas;
+
+    public void Start()
+    {
+        //Note: Setting title Id here can be skipped if you have set the value in Editor Extensions already.
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
+        {
+            PlayFabSettings.TitleId = "E8F0E"; // Please change this value to your own titleId from PlayFab Game Manager
+        }
+        PlayerPrefs.DeleteAll();
+        //var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+        // PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+        if (PlayerPrefs.HasKey("EMAIL"))
+        {
+            userEmail = PlayerPrefs.GetString("EMAIL");
+            userPassword = PlayerPrefs.GetString("PASSWORD");
+            var request = new LoginWithEmailAddressRequest { Email = userEmail, Password = userPassword };
+            PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+        }
+    }
+
+    private void OnLoginSuccess(LoginResult result)//login mi ne triba
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
+        loginCanvas.SetActive(false);
+    }
+
+    private void OnRegisterSuccess(RegisterPlayFabUserResult result)//register mi ne triba
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
+        loginCanvas.SetActive(false);
+    }
+
+    private void OnLoginFailure(PlayFabError error)//ne triba
+    {
+        var registerRequest = new RegisterPlayFabUserRequest { Email = userEmail, Password = userPassword, Username = username };
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, OnRegisterSuccess, OnRegisterFailure);
+    }
+
+    private void OnRegisterFailure(PlayFabError error)///ne triba
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    public void GetUserEmail(string emailIn)//ne triba
+    {
+        userEmail = emailIn;
+    }
+
+    public void GetUserPassword(string passwordIn)//ne triba
+    {
+        userPassword = passwordIn;
+    }
+
+    public void GetUsername(string usernameIn)
+    {
+        username = usernameIn;
+    }
+
+    public void OnClickLogin()//ne triba
+    {
+        var request = new LoginWithEmailAddressRequest { Email = userEmail, Password = userPassword };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+    }
+}
